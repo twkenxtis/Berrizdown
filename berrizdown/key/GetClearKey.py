924ff19d2f4d349bcdcc2298853750e180c33250
omenbibi <example@mail.mail>
2025-11-02 03:50:51 +0800

    bug fix wrong path

--- berrizdown/key/GetClearKey.py
+++ berrizdown/key/GetClearKey.py
@@ -68,7 +68,7 @@ async def get_clear_key(wv_pssh: str, pr_pssh: str, acquirelicenseassertion_inpu
 
         final_path = original_path_obj if original_path_obj.exists() else original_path_obj.with_name(original_path_obj.name + ".wvd")
         if len(str(wv_device_path)) < 1:
-            logger.warning(f"Widevine CDM file not found. Neither the original path nor the .wvd fallback exists: {str(final_wv_path_obj)}")
+            logger.warning(f"Widevine CDM file not found. Neither the original path nor the .wvd fallback exists: {str(final_path)}")
             return None
         wv_device_path = str(final_path)
         pssh_input: str = wv_pssh
